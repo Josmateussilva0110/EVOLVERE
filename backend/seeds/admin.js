afaa99c71c20532dbeb165f89f1bfb0621f94b89
Jose Mateus <jmateussilva044@gmail.com>
2025-09-15 13:53:22 -0300

    feat: seed para usuários administradores

---
+++ backend/seeds/admin.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcrypt")
+
+exports.seed = async function (knex) {
+  const users = [
+    {
+      username: "Mateus",
+      email: "mateus@evolvere.com",
+      password: await bcrypt.hash("admin123", 10),
+      role: 1,
+      status: 1
+    },
+    {
+      username: "Rai",
+      email: "rai@evolvere.com",
+      password: await bcrypt.hash("admin123", 10),
+      role: 1,
+      status: 1
+    },
+    {
+      username: "Lucas",
+      email: "lucas@evolvere.com",
+      password: await bcrypt.hash("admin123", 10),
+      role: 1,
+      status: 1
+    },
+    {
+      username: "Gabriel",
+      email: "gabriel@evolvere.com",
+      password: await bcrypt.hash("admin123", 10),
+      role: 1,
+      status: 1
+    }
+  ]
+
+  await knex("users")
+    .insert(users)
+    .onConflict("email") 
+    .ignore() // se já existir, não insere
+}
