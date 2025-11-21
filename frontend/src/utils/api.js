77a3398cec32bdeed1d938d980508f96efa9ba5e
Mateus Silva <127849926+Josmateussilva0110@users.noreply.github.com>
2025-11-21 11:46:41 -0300

    fix: baseUrl (#99)

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = "https://evolvere-backend.onrender.com"
 
 /**
  * Instância personalizada do Axios para realizar requisições HTTP.
