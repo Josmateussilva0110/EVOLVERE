2c93f1108ed499a2f90153575962cd56f7e7f829
Jose Mateus <jmateussilva044@gmail.com>
2025-11-28 10:56:28 -0300

    feat: download de material pelo front

--- frontend/src/components/pages/teacher/ViewDiscipline.jsx
+++ frontend/src/components/pages/teacher/ViewDiscipline.jsx
@@ -264,7 +264,7 @@ function ViewSubjectDetails() {
 
                           </div>
                           <a
-                            href={`${import.meta.env.VITE_BASE_URL}/${material.archive}`}
+                            href={`${material.file_url}`}
                             target="_blank"
                             rel="noopener noreferrer"
                             className="font-semibold text-white group-hover:text-blue-400 transition-colors flex items-center gap-2 mb-1"
@@ -274,13 +274,13 @@ function ViewSubjectDetails() {
                           </a>
                         </div>
                         <button onClick={() => deleteMaterial(material.id)}
-                          href={`${import.meta.env.VITE_BASE_URL}/${material.archive}?download=true`}
+                          href={`${material.file_url}?download=true`}
                           className="w-10 h-10 bg-red-600/80 hover:bg-red-500 rounded-xl flex items-center justify-center transition-all duration-200 group-hover:scale-110 shadow-lg border border-red-500/30"
                         >
                           <Trash className="w-4 h-4 text-white" />
                         </button>
                         <a
-                          href={`${import.meta.env.VITE_BASE_URL}/${material.archive}?download=true`}
+                          href={`${material.file_url}?download=true`}
                           className="w-10 h-10 bg-blue-600/80 hover:bg-blue-500 rounded-xl flex items-center justify-center transition-all duration-200 group-hover:scale-110 shadow-lg border border-blue-500/30"
                         >
                           <Download className="w-4 h-4 text-white" />
